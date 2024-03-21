a687e8eac548d27f22610a9cd1f6dd55843e1e1a
iysaofficial <youngscientist.iysa@gmail.com>
2024-03-21 14:48:54 +0700

    revisi hero

--- src/components/home/HeroComp.jsx
+++ src/components/home/HeroComp.jsx
@@ -5,7 +5,7 @@ const HeroComp = () =>{
             <div className="hero-container">
                 <img src="./assets/images/logo/IYCO.png" alt=""/>
                 <h1>International Youth Chemistry Olympiad</h1>
-                <p>Registration is now open for the International Youth Chemistry Olympiad! <br /> Join this prestigious event and gain an unforgettable chemical experience!
+                <p>Registration is now open for the International Youth Chemistry Olympiad! <br /> Join this prestigious event and gain an unforgettable experience!
                 </p>
                 <br />
                 <a href="https://registrasi-iyco.vercel.app/" target="_blank" className="registration-button">Registration</a>
